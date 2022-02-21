a2e9b1c8c7c4bb47034b74cae194831f2b791dd0
Timur Tül <protimu72@gmail.com>
2022-02-21 08:41:10 +0300

    kategori-crud
    Kategori CRUD işlemleri.

---
+++ AdminBlog/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using AdminBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BlogContext _context;
+        public HomeController(ILogger<HomeController> logger,BlogContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        
+        public async Task<IActionResult> AddCategory(Category category)
+        {
+            if (category.Id == 0)
+            {
+                await _context.AddAsync(category);
+
+            }
+            else
+            {
+                _context.Update(category);
+            }
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Category));
+        }
+        public IActionResult Category()
+        {
+            List<Category> list = _context.Category.ToList();
+            return View(list);
+        }
+        public async Task<IActionResult> CategoryDetails(int Id)
+        {
+            var category = await _context.Category.FindAsync(Id);
+            return Json(category);
+        }
+
+        public async Task<IActionResult> DeleteCategory(int? id)
+        {
+            Category category = await _context.Category.FindAsync(id);
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Category));
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
